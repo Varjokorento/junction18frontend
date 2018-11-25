d0ee40e1b1449dff82322934258e9343fd1e1712
Kalle Pusa <kalle.pusa@gmail.com>
2018-11-25 10:43:43 +0200

    Hotain

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ feedBack = () => {
     <div className="custom">
       
     <div className="text-center">
-      <h1 className="text-center margin-bottom">Mechelininkatu</h1>
+      <h2 className="text-center margin-bottom">Mechelininkatu</h2>
     {!this.state.feedBackClicked ? (
       <div>
         <h2 className="text-center margin-bottom margin-top">Projektin tila nyt</h2>
